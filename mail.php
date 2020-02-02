91daa02f58f639f10da3636cfa47a02b2fceddfd
Siddhant kumar Mishra <47280736+Siddhantk123@users.noreply.github.com>
2020-02-02 10:35:26 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+$to="mishra.siddhant003@gmail.com";
+$subject="Response form website";
+$message="i am very thankful to you";
+$header="from: mishra.adarsh2111@gmail.com";
+
+
+if(mail($to, $subject, $message, $header)){
+	echo "Mail send successfuly";
+}
+else{
+	echo "canno send mail";
+}
+
+?>
