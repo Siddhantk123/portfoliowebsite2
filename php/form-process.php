d6503d387b46f90e305fcc5398791ac6464c6b2d
Siddhant kumar Mishra <47280736+Siddhantk123@users.noreply.github.com>
2020-02-02 10:02:02 +0530

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -40,7 +40,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "armanmia7@gmail.com";
+$EmailTo = "mishra.siddhant003@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
